5e014c3a192c86d0729d18cf55f470c8b1cc7fa8
Brian Werts <39464171+bwerts@users.noreply.github.com>
2024-05-23 11:51:20 -0500

    Update testscript.sh

--- scripts/testscript.sh
+++ scripts/testscript.sh
@@ -3,4 +3,4 @@
 dnf install epel-release glibc-langpack-en -y
 dnf install ansible -y
 
-/usr/bin/ansible-playbook /root/ansible-new/compute_img.yaml
+/usr/bin/ansible-playbook /root/ww_cluster/ansible/compute_img.yaml
