e19aac532b16489b512ce618390788caa541769a
Brian Werts <39464171+bwerts@users.noreply.github.com>
2024-05-23 11:51:54 -0500

    Update autotest.sh

--- scripts/autotest.sh
+++ scripts/autotest.sh
@@ -2,4 +2,4 @@
 
 wwctl container copy rocky8.9-base rocky8.9_prod00
 
-wwctl container exec --bind /root:/root rocky8.9_prod00 /bin/bash /root/testscript.sh
+wwctl container exec --bind /root:/root rocky8.9_prod00 /bin/bash /root/ww_cluster/scripts/testscript.sh
